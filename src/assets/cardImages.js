559d893cc7d5b7455e4dc3029763fdf03665eec9
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-04-10 17:46:18 -0400

    Update file path for deployment

---
+++ src/assets/cardImages.js
@@ -0,0 +1,20 @@
+import abstract1 from "/imgs/blue-gold-liquid-marble.png";
+
+import abstract2 from "/imgs/macro-red.png";
+
+import abstract3 from "/imgs/orange-blue-liquid-marble.png";
+
+import abstract4 from "/imgs/orange-green-smear.png";
+
+import abstract5 from "/imgs/peacock.png";
+
+import abstract6 from "/imgs/rice-terraces.png";
+
+export const cardImages = [
+  { src: abstract1, id: 1 },
+  { src: abstract2, id: 2 },
+  { src: abstract3, id: 3 },
+  { src: abstract4, id: 4 },
+  { src: abstract5, id: 5 },
+  { src: abstract6, id: 6 },
+];
