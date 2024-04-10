93497ae99fdc74000f91a74ddeffe39689d88db2
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-04-10 17:43:35 -0400

    Add effect hook for record tracking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,90 @@
 import { useState, useEffect } from "react";
-import { cardImages } from "./cardImages.js";
+import { cardImages } from "../assets/cardImages.js";
 import { Card } from "./Card.jsx";
-
-import background from "../../public/background.png";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [cards, setCards] = useState([]);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disabled, setDisabled] = useState(false);
+  const [cardOne, setCardOne] = useState();
+  const [cardTwo, setCardTwo] = useState();
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const [turns, setTurns] = useState(0);
+  const [matches, setMatches] = useState(0);
   const [record, setRecord] = useState(0);
 
-  const handleClick = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-  };
+  //Initiate a new game on initial page render:
 
   useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      setDisabled(true);
+    resetGame();
+  }, []);
 
-      if (choiceOne.src === choiceTwo.src) {
-        console.log("matched");
+  const handleClick = (card) => {
+    cardOne ? setCardTwo(card) : setCardOne(card);
+  };
+
+  //Check card 1 and card 2 for a match:
+
+  useEffect(() => {
+    if (cardOne && cardTwo) {
+      setIsDisabled(true);
+
+      if (cardOne.id === cardTwo.id) {
+        setMatches((prevCount) => prevCount + 1);
 
         setCards((prevCards) => {
           return prevCards.map((card) => {
-            if (card.src === choiceOne.src) {
+            if (card.id === cardOne.id) {
               return { ...card, matched: true };
             } else {
-              return card;
+              return { ...card };
             }
           });
         });
         resetTurn();
       } else {
-        setTimeout(() => resetTurn(), 1000);
+        setTimeout(() => resetTurn(), 2000);
       }
     }
-  }, [choiceOne, choiceTwo]);
+  }, [cardOne, cardTwo]);
 
-  //Reset game:
+  //Keep track of best score:
+
+  useEffect(() => {
+    if (matches === 6) {
+      if (record === 0 || record > turns) {
+        setRecord(turns);
+        setMatches(0);
+      }
+    }
+  }, [matches]);
+
+  //Reset card choices:
+
+  const resetCards = () => {
+    setCardOne(null);
+    setCardTwo(null);
+  };
+
+  //Start a new game:
+
   const resetGame = () => {
     const unshuffledCards = [...cardImages, ...cardImages];
     const shuffledCards = shuffleCards(unshuffledCards);
     const gameCards = shuffledCards.map((card) => ({
       ...card,
-      id: Math.random(),
+      key: uuidv4(),
       matched: false,
     }));
 
-    setChoiceOne(null);
-    setChoiceTwo(null);
+    resetCards();
     setCards(gameCards);
     setTurns(0);
   };
 
-  //Use Fisher Yates shuffle algorithm to randomize card order:
+  //Use Fisher Yates shuffle algorithm to
+  //randomize card order:
+
   function shuffleCards(array) {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
@@ -65,46 +93,38 @@ function App() {
     return array;
   }
 
-  // reset choices and increase turn.
+  // Reset cards and increase turn:
+
   const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
+    resetCards();
     setTurns((prevTurn) => prevTurn + 1);
-    setDisabled(false);
+    setIsDisabled(false);
   };
 
-  //start a new game on initial page render.
-
-  useEffect(() => {
-    resetGame();
-  }, []);
-  //Component:
-
   return (
-    <div>
-      <header>Abstract Art Memory Game</header>
+    <div className="wrapper">
+      <header>
+        <h1>Abstract Art Memory Game</h1>
+      </header>
 
       <div className="app">
         <section className="game">
           <div className="card-grid">
             {cards.map((card) => (
               <Card
-                key={card.id}
+                key={card.key}
                 card={card}
-                flipped={
-                  card === choiceOne || card === choiceTwo || card.matched
-                }
+                flipped={card.matched || card === cardOne || card === cardTwo}
                 handleClick={handleClick}
-                isDisabled={disabled}
+                isDisabled={isDisabled}
               ></Card>
             ))}
-            ;
           </div>
         </section>
 
         <section className="game-stats">
-          <div className="turns">Turns</div>
-          <div className="record">Record</div>
+          <div className="turns">Turns: {turns}</div>
+          <div className="record">Record: {record}</div>
           <button onClick={resetGame}>New Game</button>
         </section>
       </div>
